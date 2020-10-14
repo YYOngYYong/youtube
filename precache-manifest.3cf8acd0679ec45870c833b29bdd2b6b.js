9b38052dd6f9a4a218981bb847db6863a8085041
gwanyong_04 <43106908+YYOngYYong@users.noreply.github.com>
2020-10-14 12:49:24 +0900

    Updates

---
+++ precache-manifest.3cf8acd0679ec45870c833b29bdd2b6b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d7f09b3ea50b445da8b533617bf6f71",
+    "url": "/youtube/index.html"
+  },
+  {
+    "revision": "ddeb9a6421d548770974",
+    "url": "/youtube/static/css/main.c585509b.chunk.css"
+  },
+  {
+    "revision": "5d2033e0c8bb2dd6501d",
+    "url": "/youtube/static/js/2.2cbe567c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youtube/static/js/2.2cbe567c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddeb9a6421d548770974",
+    "url": "/youtube/static/js/main.c65c3634.chunk.js"
+  },
+  {
+    "revision": "66d15f02628b16631539",
+    "url": "/youtube/static/js/runtime-main.fd6ec26e.js"
+  }
+]);
